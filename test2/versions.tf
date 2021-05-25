3f75af4b86a4d7bf03c3fce8a59559d99011b175
hitsumabushi845 <shimada.kento8974@gmail.com>
2021-05-26 01:00:09 +0900

    test

--- test2/versions.tf
+++ test2/versions.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     google = {
-      source  = "hashicorp/google"
-      version = "~> 3.64.0"
+      source  =   "hashicorp/google"
+      version   = "~> 3.64.0"
     }
   }
   required_version = ">= 0.14"
