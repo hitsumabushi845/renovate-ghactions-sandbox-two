d5acefe217ef5d2f2697b688252870c4d8e8ccc2
Sample Action <sample@example.com>
2021-05-25 16:01:07 +0000

    Format tf files

--- test1/versions.tf
+++ test1/versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version   = "~> 3.65.0"
+      version = "~> 3.65.0"
     }
   }
-  required_version =   ">= 0.14"
+  required_version = ">= 0.14"
 }
